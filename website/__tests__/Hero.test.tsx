b8589fa0d489e3760aba32f338d52019748915d1
amayavilayvong <amaya.c.vilayvong@gmail.com>
2025-04-04 13:48:14 +0000

    added subhead, cta, and image test

--- website/__tests__/Hero.test.tsx
+++ website/__tests__/Hero.test.tsx
@@ -10,4 +10,25 @@ describe('Hero', () => {
  
     expect(heading).toBeInTheDocument()
   })
+
+  //Test the rendering/inclusion of the subheading
+  it('renders the subheading', () => {
+    render(<Hero />)    
+    const subheading = screen.getByText(/explore our cutting-edge dashboard/i)  //Look for a paragraph of text under the heading
+    expect(subheading).toBeInTheDocument()
+  })
+
+  //Test the rendering/inclusion of the CTA (go now) button
+  it('renders the CTA', () => {
+    render(<Hero />)
+    const button = screen.getByRole('button', { name: /go now/i })    //Look for the button that says "Go now"
+    expect(button).toBeInTheDocument()
+  })
+
+  //Test the rendering/inclusion of the Hero visual
+  it('renders the Hero visual', () => {
+    render(<Hero />)
+    const heroVisual = screen.getByTestId('hero-visual')  //Look for the image/background element with hero-visual as its id
+    expect(heroVisual).toBeInTheDocument()
+  })
 })
